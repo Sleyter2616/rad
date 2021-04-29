5a69ff8a8297245df27ca2e06efa7b4de9502429
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-04-29 18:26:03 -0400

    added an error, updated state variables for inputs and also made itso we can add result to table

--- src/Screens/TableScreen.js
+++ src/Screens/TableScreen.js
@@ -1,28 +1,90 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import {Table, Container} from 'react-bootstrap'
+import {Table, Container, Form, Button} from 'react-bootstrap'
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 import TableRow from '../components/TableRow'
 
 const TableScreen = () => {
 	const apiURL = 'https://jsonplaceholder.typicode.com/todos'
 
-	const [results, setResults] = useState({})
+	const [userId, setUserId] = useState(0)
+	const [title, setTitle] = useState('')
+	const [completed, setCompleted] = useState(false)
+	const [results, setResults] = useState([])
+	const [error, setError] = useState([])
+
 	const callApi = async () => {
 		const {data} = await axios.get(apiURL)
+
 		setResults(data)
+	}
+	const submitHandler = (e) => {
+		e.preventDefault()
+		if (title === '') {
+			setError('Cannot leave title empty')
+		} else if (results.length > 0) {
+			setError('')
+			const newId = results[results.length - 1].id + 1
+			const newResult = {userId, title, completed, id: newId}
+			let newResults = results
+			newResults.push(newResult)
+			console.log('UPDATING RESULT')
+			setResults(newResults)
+			setUserId(0)
+			setTitle('')
+			setCompleted(false)
+		}
 	}
 	const onDragEnd = (param) => {
 		const sourceIndex = param.source.index
 		const destinationIndex = param.destination.index
 		let res = results
 		res.splice(destinationIndex, 0, res.splice(sourceIndex, 1)[0])
-		console.log(res)
 		setResults(res)
 	}
+	// useEffect(() => {
+	// 	console.log('results changed')
+	// }, [results])
 	return (
 		<div>
-			<button onClick={callApi}>Call API</button>
+			{results.length === 0 && (
+				<Button onClick={callApi}>Call API</Button>
+			)}
+			{error !== '' && <h2>{error}</h2>}
+			{results.length > 0 && (
+				<Form onSubmit={submitHandler}>
+					<Form.Group controlId='userId'>
+						<Form.Label>UserId</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Enter UserId'
+							value={userId}
+							onChange={(e) => setUserId(e.target.value)}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId='title'>
+						<Form.Label>Title</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Enter Title'
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId='completed'>
+						<Form.Label>Completed</Form.Label>
+						<Form.Check
+							type='checkbox'
+							placeholder='Enter Completed'
+							value={completed}
+							onChange={(e) => setCompleted(e.target.value)}
+						></Form.Check>
+					</Form.Group>
+					<Button type='submit' variant='primary'>
+						Add to results
+					</Button>
+				</Form>
+			)}
 			<DragDropContext
 				onDragEnd={(param) => {
 					onDragEnd(param)
